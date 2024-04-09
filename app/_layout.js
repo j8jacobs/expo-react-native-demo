0fb2e2e482eb6701c7e00ebd9f751db7590ccbc6
Julian Jacobs <jljacobs45@gmail.com>
2024-04-09 10:51:15 -0700

    correct structure for single page nav

--- app/_layout.js
+++ app/_layout.js
@@ -1,39 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import Constants from "expo-constants";
-import { Link } from "expo-router";
+import { Stack } from "expo-router/stack";
 
-export default function App() {
-  return (
-    <SafeAreaProvider style={styles.frameContainer}>
-      <StatusBar style="auto" />
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <Link href="/purpose">Purpose</Link>
-        </View>
-      </View>
-    </SafeAreaProvider>
-  );
+export default function Layout() {
+  return <Stack />;
 }
-
-const styles = StyleSheet.create({
-  frameContainer: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    paddingTop: 20,
-  },
-  card: {
-    width: "90%",
-    height: 200,
-    borderWidth: 1,
-    margin: 12,
-    padding: 12,
-    borderRadius: 8,
-  },
-});
--- app/purpose.js
+++ app/purpose.js
@@ -2,13 +2,16 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Constants from "expo-constants";
+import { Link } from "expo-router";
 
-export default function Purpose() {
+export default function App() {
   return (
     <SafeAreaProvider style={styles.frameContainer}>
       <StatusBar style="auto" />
       <View style={styles.container}>
-        <Text>Hello World</Text>
+        <View style={styles.card}>
+          <Link href="/purpose">Purpose</Link>
+        </View>
       </View>
     </SafeAreaProvider>
   );
@@ -24,5 +27,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     paddingTop: 20,
+  },
+  card: {
+    width: "90%",
+    height: 200,
+    borderWidth: 1,
+    margin: 12,
+    padding: 12,
+    borderRadius: 8,
   },
 });
