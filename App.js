571f54df43cd375007291db7af58d0ac5238c3ae
Julian Jacobs <jljacobs45@gmail.com>
2024-04-09 10:08:40 -0700

    safe area resolved

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Constants from "expo-constants";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider style={styles.frameContainer}>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
@@ -14,10 +15,13 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  frameContainer: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
