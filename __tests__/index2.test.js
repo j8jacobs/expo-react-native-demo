b85c18a6056fc4409b3acd6dcd62b04b1cc0da94
Julian Jacobs <jljacobs45@gmail.com>
2024-04-10 13:25:56 -0700

    import issue resolved, still don't know how to test the router

--- __tests__/index2.test.js
+++ __tests__/index2.test.js
@@ -1,7 +1,36 @@
 import { renderRouter, screen } from "expo-router/testing-library";
+import { View } from "react-native";
+import App from "../app/index";
 
 describe("2 <App />", () => {
   it("should test routing", () => {
     console.log("theer...");
+    const MockComponent = jest.fn(() => <View />);
+
+    renderRouter(
+      {
+        index: MockComponent,
+        "directory/a": MockComponent,
+        "(group)/b": MockComponent,
+      },
+      {
+        // initialUrl: "/directory/a",
+        initialUrl: "/(group)/a",
+      }
+    );
+
+    expect(screen).toHavePathname("/(group)/a");
+  });
+
+  xit("should test actual routing", () => {
+    const output = renderRouter(
+      {
+        "/": App,
+      },
+      {
+        initialUrl: "/",
+      }
+    );
+    console.log(output);
   });
 });
